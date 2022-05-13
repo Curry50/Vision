c338fdf96081e8f7efc0c4ce4a23b4115ff22d4f
Curry50 <98143304+Curry50@users.noreply.github.com>
2022-05-13 11:28:59 +0800

    Update VideoCapture.py

--- Final/VideoCapture.py
+++ Final/VideoCapture.py
@@ -1,13 +1,18 @@
+#导入相关模块
 import cv2
 
+#定义VideoCapture类
 class VideoCapture:
+    #捕捉画面
     def Capture(self,CameraIndex):
         cap =cv2.VideoCapture(CameraIndex)
         return cap
     
+    #设置画面相关参数
     def CaptureSet(self,Cap,Index,Size):
         Cap.set(Index,Size)
-        
+    
+    #读取画面
     def CaptureRead(self,Cap):
         ret,frame = Cap.read()
         return ret,frame
