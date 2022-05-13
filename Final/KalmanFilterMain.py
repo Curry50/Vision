5264bf34ed85ebb986281560a805c50ccce59d3b
Curry50 <98143304+Curry50@users.noreply.github.com>
2022-05-13 10:59:09 +0800

    Update KalmanFilterMain.py

--- Final/KalmanFilterMain.py
+++ Final/KalmanFilterMain.py
@@ -27,29 +27,33 @@ except:
 #主循环
 while True:
     ret, frame = vc.CaptureRead(cap) #读取画面
-    frame = cv2.resize(frame,None,fx=1/5,fy=16/45) #将图像的分辨率缩放至256*256
+    frame = cv2.resize(frame,None,fx=1/5,fy=16/45) #将图像的分辨率缩放至256*256，便于通信传输数据
     if ret is False:
         break
-    green_bbox = od.detect(frame)
-    data_x = []
+    green_bbox = od.detect(frame) #获取检测到的矩形包围框的x,y,w,h信息
+    
+    #建立列表，储存检测到的装甲板的两个有色灯条的x,y,w,h信息
+    data_x = [] 
     data_y = []
     data_x2 = []
     data_y2 = []
+    
+    #遍历两个有色灯条的x,y,w,h信息
     for i in range(len(green_bbox)):
         x,y,x2,y2 = green_bbox[i]
         data_x.append(x)
         data_y.append(y)
         data_x2.append(x2)
         data_y2.append(y2)
-        cv2.rectangle(frame, (x, y), (x2, y2), (0, 0, 255), 4)
+        cv2.rectangle(frame, (x, y), (x2, y2), (0, 0, 255), 4) #绘出灯条的红色矩形包围框
     
     try:
-        Xcenter = int((data_x[0] + data_x[1])/2)
-        Ycenter = int((data_y[0]-(data_y[0]-data_y2[0])/2 + data_y[1]-(data_y[1]-data_y2[1])/2 )/2)
-        predicted = kf.predict(Xcenter,Ycenter)
-        cv2.circle(frame,(Xcenter,Ycenter),10,(0,0,255),-1)
-        cv2.circle(frame,(predicted[0],predicted[1]),10,(255,0,0),-1)
-        sc.dataProcess(ser,predicted[0],predicted[1])
+        Xcenter = int((data_x[0] + data_x[1])/2) #求取装甲板中心横坐标
+        Ycenter = int((data_y[0]-(data_y[0]-data_y2[0])/2 + data_y[1]-(data_y[1]-data_y2[1])/2 )/2) #求取装甲板中心纵坐标
+        predicted = kf.predict(Xcenter,Ycenter) #利用卡尔曼滤波预测中心坐标
+        cv2.circle(frame,(Xcenter,Ycenter),10,(0,0,255),-1) #绘出红色实际中心点
+        cv2.circle(frame,(predicted[0],predicted[1]),10,(255,0,0),-1) #绘出蓝色预测中心点
+        sc.dataProcess(ser,predicted[0],predicted[1]) #处理预测数据并向stm32发送预测中心点坐标
     except:
         print("No Armor")
 
